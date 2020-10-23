b16909f60e9109b5e2a5b7d2f2fc24a12cef6e03
Steven <sjecmen@gmail.com>
2020-10-23 15:14:38 -0400

    Initial

---
+++ low_rank_flag.py
@@ -0,0 +1,31 @@
+import numpy as np
+import scipy.linalg
+
+# Threshold low singular values of S so it has given rank.
+def threshold(S, rank):
+    U, s, Vh = scipy.linalg.svd(S, full_matrices=False)
+    k = s.size
+    if k < rank:
+        return S
+    for i in range(rank, k):
+        s[i] = 0
+    return U @ np.diag(s) @ Vh
+    
+# Flag the top n_flag entries in S based on the difference with the 
+# thresholded matrix at rank. M is the mask matrix.
+def flag(S, M, rank, n_flag):
+    L = threshold(S, rank)
+    diff = np.abs(L - S)
+    flat_idxs = np.argsort(diff, axis=None)
+    unraveled = np.unravel_index(flat_idxs, S.shape)
+    idxs = [(unraveled[0][i], unraveled[1][i]) for i in range(flat_idxs.size)]
+    flags = []
+    for idx in reversed(idxs):
+        if M[idx] == 0 and S[idx] == 1:
+            flags.append(idx)
+            n_flag -= 1
+            if n_flag == 0:
+                break
+    return flags
+
+
---
+++ test.py
@@ -0,0 +1,52 @@
+from low_rank_flag import *
+import numpy as np
+import matplotlib.pyplot as plt
+import sys
+
+# Return the probability of detecting the manipulation (averaged over 
+# all r,p) when flagging nflag bids using a threshold at rank.
+def run(rank, nflag):
+    correct = 0
+    i = 0
+    for r in range(S.shape[0]):
+        for p in range(S.shape[1]):
+            if M[r, p] == 1:
+                continue
+            '''
+            # uncomment for weaker manipulation case
+            if S[r, p] == 1:
+                continue
+            '''
+
+            Sm = np.copy(S)
+            Sm[r, :] = 0 # comment for weaker manipulation case
+            Sm[r, p] = 1
+            flags = flag(Sm, M, rank, nflag)
+            for f in flags:
+                if f == (r, p):
+                    correct += 1
+                    break
+            i += 1
+    return correct/i
+
+f = np.load("data/preflib1.npz")
+S, M = f['similarity_matrix'], f['mask_matrix']
+
+print("total rank", np.linalg.matrix_rank(S))
+
+nflag = 3#int(S.size * 0.05)
+print("number flags", nflag)
+
+ranks = np.arange(1, np.linalg.matrix_rank(S))
+ps = []
+for rank in ranks:
+    p = run(rank, nflag)
+    ps.append(p)
+    print(rank, p)
+
+# plotting
+plt.plot(ranks, ps, '-o')
+plt.xlabel('rank threshold')
+plt.ylabel('detection probability')
+plt.ylim([0, 1])
+plt.savefig('bidexp_'+str(nflag) +'.png')
